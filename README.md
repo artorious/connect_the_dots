084a8e7179fee58b822df558d9ff346e6b015884
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-21 10:44:40 +0300

    Add stubbed functions for game engine

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 A simplified version of of the connect the dots game
 
+<img alt="Connect the dot game in progress." style="border-width:0" src="static/dot_3x3_in_progress.png" />
 
+# Python3 Topics covered
+
+Separating concerns with pluggable modules
+ 
 Attribution
 ===========
 “Fundamentals of Python Programming”, &copy;2016 Richard L. Halterman.
--- dot_3x3_logic.py
+++ dot_3x3_logic.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""dot_3x3_logic.py
-Game Engine
+"""Game Engine 
 
 Provides the logic for a simplified connect the dots game.
 The code in this module determines and enforces the rules of the game.
@@ -19,7 +18,7 @@ The game allows two players, X and Y, to alternately add horizontal and
 vertical lines to connect the dots. The following shows a game in progress:
 
         @---@   @
-            |   |
+            |	|
         @   @   @
                 |
         @   @---@
@@ -29,7 +28,7 @@ control of the next turn. The following shows a game in which player Y has
 completed a square:
 
         @---@   @
-            |   |
+            |	|
         @   @---@
             | Y |
         @   @---@
@@ -54,7 +53,7 @@ This game engine manages 12 lines. Each line distinguished by its name
         |                       |                       |
         |                       |                       |
         |                       |                       |
- 'West_Southwest'         'Center_South'         'East_Southeast'
+'West_Southwest'         'Center_South'         'East_Southeast'
         |                       |                       |
         |                       |                       |
         |                       |                       |
@@ -106,13 +105,107 @@ _w_c, _e_c = False, False
 _w_sw, _s_c, _s_se = False, False, False
 _s_sw, _s_se = False, False
 
-# Current player - A string, 'X' for player X or 'Y' for player Y
+# Init Current player - A string, 'X' for player X or 'Y' for player Y
 _current_player = 'X'
 
-# Initsquare ownership as None
-# A string reps ownership, 'X' for player X or 'Y' for player Y
+# Init square ownership as None
+#TODO: A string reps ownership, 'X' for player X or 'Y' for player Y
 
 _top_left_owner, _top_right_owner = None, None
 _bottom_left_owner, _bottom_right_owner = None, None
 
+##############################################################################
+# Note: Private Functions (prefixed with underscore)
+# Functions accessed only within this module
+#-----------------------------------------------------------------------------
 
+def _update_square(sq):	
+    """(str) -> turtle, bool
+    Updates the owner of square <sq>, if possible.
+    <sq> must one of the strings; 'top_left', 'top_right', 'bottom_left',
+    or 'bottom_right'. 
+    Function Checks to see:
+        1. If the square currently is not owned by a player and
+        2. if all the lines are in place to complete the sqaure.
+    
+    If both conditions are met, it marks the square with the current player
+    and returns True. 
+    If one of the players already owns the square, or if not all lines exist 
+    to complete the square, returns False. 		
+    """        
+    return
+
+def _update_squares():
+    """() -> turtle, bool
+    Attepts to update the owners of all the squares that a new line might 
+    affect. 
+    
+    Returns True if one or more squares receives a new owner, otherwise False
+    """
+    return
+
+##############################################################################
+
+##############################################################################
+# NOTE: Functions that reveal or control the state of the current game.
+# These functions are meant to be accessed outside of this module
+#-----------------------------------------------------------------------------
+
+def add_line(line):
+    """(str) -> turtle, bool
+    Attempts to add a line between two dots. 
+    The parameter <line> must be one of 'North_Northeast', 'North_Northwest',
+    'East_Center', etc, (A string represnting a line on the game board)
+    
+    If the line is not present, adds the line and returns True.
+    If the line is already present, no change to state of game board and
+    returns False
+    """
+    return
+
+def square_owner(sq):
+    """ (str) -> turtle, NoneType
+    Checks who owns the given square <sq>
+    <sq> must be one of the strings, 'top_left', 'top_right', 'bottom_right'
+    or 'bottom_left'
+    
+    Returns the player who owns the given square, and None if noone owns it
+    """
+    return
+
+def check_line(line):
+    """(str) -> bool
+    Checks whether the line <line> exists. 
+    The parameter <line> must be one of 'North_Northeast', 'North_Northwest',
+    'East_Center', etc, (A string represnting a line on the game board)
+
+    Returns True if the line exists on the game board, otherwise False if it
+    doesn't exist yet
+    """
+    return
+
+def winner():
+    """() -> str, NoneType
+    Declares the game's winner, player X, player Y or a draw
+    
+    Returns 'X' or 'Y', or 'Draw' if the game board is full and  both players
+    each own two squares.
+    Reurns None if open squares still exist, and so the game can continue
+    """
+    return
+
+def initialize_board():
+    """
+    Makes the playing board ready for a new game.
+        1. Clears all the lines from the board,
+        2. makes all the squares empty, and
+        3. sets the current player to X
+    Does not return a value to the caller
+    """
+    return
+
+def current_player():
+    """() -> str
+    Returns the player whose turn it is to move
+    """
+    return
